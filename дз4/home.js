cb5ab72bea478130b79990ccc05a0f4267ee1081
maksatmkkk <143606944+maksatmkkk@users.noreply.github.com>
2023-08-30 17:47:47 +0600

    Add files via upload

---
+++ дз4/home.js
@@ -0,0 +1,35 @@
+let buttonCountPlus = document.getElementById("buttonCountPlus");
+let buttonCountMinus = document.getElementById("buttonCountMinus");
+let count = document.getElementById("buttonCountNumber");
+let count2 = document.getElementById("num");
+let number = 1;
+let price = 350;
+
+buttonCountPlus.onclick = function() {
+    if (number <= 333) {
+        number++;
+        count.innerHTML = number;
+        count2.value = number * price;
+    }
+};
+
+buttonCountMinus.onclick = function() {
+   if (number >= 2) {
+       number--;
+       count.innerHTML = number;
+       count2.value = number * price;
+    }
+}
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
